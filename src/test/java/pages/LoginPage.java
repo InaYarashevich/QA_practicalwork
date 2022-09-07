6b9f6f73daf41b1131b3d0c4dbcc574176cd992f
InaYarashevich <ina.yarashevich@gmail.com>
2022-09-07 22:28:30 +0300

    refactoring

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -8,11 +8,10 @@ public class LoginPage extends Base {
     private static final String PASSWORD_CSS = "input[data-qa-id='login-form-password']";
     private static final String LOGIN_BUTTON_CSS = "#login-submit-btn";
 
-    public void login(String email, String password) {
-
+    public void login(String email, String password) throws InterruptedException {
         driver.findElement(By.cssSelector(EMAIL_CSS)).sendKeys(email);
         driver.findElement(By.cssSelector(PASSWORD_CSS)).sendKeys(password);
+        Thread.sleep(5000);
         driver.findElement(By.cssSelector(LOGIN_BUTTON_CSS)).click();
-
     }
 }
